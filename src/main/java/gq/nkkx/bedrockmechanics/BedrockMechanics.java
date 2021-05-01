c31ae8f11ded108a6995429679ec802688eb7d9b
nekkan <nxkkdev@gmail.com>
2021-05-01 17:29:54 -0300

    feat: Add gamecontrollerdb.txt

--- src/main/java/gq/nkkx/bedrockmechanics/BedrockMechanics.java
+++ src/main/java/gq/nkkx/bedrockmechanics/BedrockMechanics.java
@@ -1,7 +1,26 @@
 package gq.nkkx.bedrockmechanics;
 
-public class BedrockMechanics {
+import gq.nkkx.bedrockmechanics.options.BedrockMechanicsOptions;
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class BedrockMechanics implements ModInitializer {
 
     public static final String MOD_ID = "bedrock-mechanics";
+    private static final BedrockMechanicsOptions options = new BedrockMechanicsOptions();
+
+    public static BedrockMechanicsOptions options() {
+        return options;
+    }
+
+    public static Logger getLogger() {
+        return LogManager.getLogger("gq.nkkx.bedrockmechanics.BedrockMechanics");
+    }
+
+    @Override
+    public void onInitialize() {
+        // no-op
+    }
 
 }
