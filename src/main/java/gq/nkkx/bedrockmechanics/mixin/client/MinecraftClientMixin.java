86a40a3d9981269a4545eaf49304752e1ea7a888
nekkan <nxkkdev@gmail.com>
2021-05-04 06:47:32 -0300

    feat: Create IMinecraftClient.java

--- src/main/java/gq/nkkx/bedrockmechanics/mixin/client/MinecraftClientMixin.java
+++ src/main/java/gq/nkkx/bedrockmechanics/mixin/client/MinecraftClientMixin.java
@@ -1,17 +1,23 @@
 package gq.nkkx.bedrockmechanics.mixin.client;
 
 import gq.nkkx.bedrockmechanics.client.BedrockMechanicsClient;
+import gq.nkkx.bedrockmechanics.client.accessor.IMinecraftClient;
 import gq.nkkx.bedrockmechanics.client.controller.input.ControllerInputManager;
 import gq.nkkx.bedrockmechanics.client.controller.tasks.ControllerUpdateThread;
 import gq.nkkx.bedrockmechanics.client.controller.tasks.ControllersMappingsSetup;
 import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
-public class MinecraftClientMixin {
+public abstract class MinecraftClientMixin implements IMinecraftClient {
+
+    @Override
+    @Accessor("currentFps")
+    public abstract int getCurrentFPS();
 
     @Inject(method = "<init>", at = @At("RETURN"))
     private void bedrock_mechanics$init(CallbackInfo callbackInfo) {
