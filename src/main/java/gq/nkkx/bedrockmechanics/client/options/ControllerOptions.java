3c6119723c6f5033730b08546c7a38d8b2d2376c
nekkan <nxkkdev@gmail.com>
2021-05-04 22:25:19 -0300

    feat: Add "bedrock-unicode-characters" submodule

--- src/main/java/gq/nkkx/bedrockmechanics/client/options/ControllerOptions.java
+++ src/main/java/gq/nkkx/bedrockmechanics/client/options/ControllerOptions.java
@@ -1,6 +1,7 @@
 package gq.nkkx.bedrockmechanics.client.options;
 
 import com.google.gson.annotations.SerializedName;
+import gq.nkkx.bedrockmechanics.BedrockMechanics;
 import gq.nkkx.bedrockmechanics.client.controller.Controller;
 import gq.nkkx.bedrockmechanics.registry.ControllerRegistry;
 import lombok.Data;
@@ -22,11 +23,15 @@ public class ControllerOptions {
     private float deadZone;
 
     public Controller getSelectedController() {
-        try {
-            return ControllerRegistry.getInstance().withId(selectedControllerId).orElse(null);
-        } catch (Exception ignored) {
+        if (selectedControllerId == -1) {
             return null;
         }
+        return ControllerRegistry.getInstance().withId(selectedControllerId).orElse(null);
+    }
+
+    public void setSelectedController(Controller controller) {
+        BedrockMechanics.getLogger().info("Setting current controller to " + controller.getId());
+        setSelectedControllerId(controller.getId());
     }
 
 }
